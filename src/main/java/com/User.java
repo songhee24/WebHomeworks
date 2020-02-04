baff83dc03e8baa6023d6ab1316dd212f8248683
Azamat Orozbaev <44497623+songhee24@users.noreply.github.com>
2020-02-04 11:38:47 +0600

    homework49.2

---
+++ src/main/java/com/User.java
@@ -0,0 +1,50 @@
+package com;
+
+public class User{
+   private  String login;
+   private  String password;
+   private String email;
+   private String gender;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
